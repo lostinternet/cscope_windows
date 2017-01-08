0db5b6799c64e35e0bd5defe6a8ca6e6432baf3b
Charlie Ding <82871021@163.com>
2017-01-08 19:13:25 +0800

    Create cscope-indexer.bat

---
+++ cscope-indexer.bat
@@ -0,0 +1,19 @@
+::Author:Charlie Ding
+::Copyright Charlie Ding
+::But you can use this file by free
+
+@echo off
+set _cur_dir=%cd%
+echo %_cur_dir%
+
+::you can add the file ext in this var
+set _file_list= *.h *.c *.cpp
+
+::generate cscope.files
+dir /s/b/a-d %_file_list% >cscope.files
+
+::generate cscope.out
+::at first you should put the cscope.exe to system directory.
+cscope.exe -bCkR -i cscope.files
+
+goto :eof
